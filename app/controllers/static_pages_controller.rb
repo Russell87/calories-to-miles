6313148e775773854a78b7aa6cdf4acbe31c74ee
Russell <russellchristensen@me.com>
2015-05-12 00:27:40 +0000

    Committing before adding a new model for exercises

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,13 @@
 class StaticPagesController < ApplicationController
   def homepage
+    
+
   end
+  
+  def results
+    
+    @result = params[:answer][:answer1].to_i * 5
+    
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  
+  match 'results', to: 'static_pages#results', via: 'post', as: 'results'
   get 'foods/new'
 
   root                 'static_pages#homepage'
   get 'about'       => 'static_pages#about'
   get 'settings'    => 'static_pages#settings'
   get 'food' => 'foods#show'
+ 
   
   #This is a shortcut to all the routes
   
