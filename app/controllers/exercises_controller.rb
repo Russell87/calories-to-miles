3c5bf3bff24fef192194c9d50a1b8e30aef0a7cf
Russell <russellchristensen@me.com>
2015-05-17 21:43:08 +0000

    Fully functioning form with calculated results

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -12,12 +12,19 @@ class ExercisesController < ApplicationController
    def results
       
       #find everything by ID!!! 
-      @weightResult = params[:results][:weight] #shows weight!! params is the form it came from and weight is the variable
-      @exerciseResult = Exercise.find_by_id(params[:results][:exercise].to_i) #this might need to be met not exercise
-      @foodResult = Food.find_by_id(params[:results][:food].to_i) #this also might need to be changed to calories
+      @weightResult = params[:results][:weight].to_i / 2.2046 #shows weight!! params is the form it came from and weight is the variable
+      @exerciseResult = Exercise.find_by_id(params[:results][:exercise].to_i) 
+      @foodResult = Food.find_by_id(params[:results][:food].to_i) 
       
       #math calcs here
       #@weightResult * @exterciseResult + @foodResult ete etc
-
+      
+      
+      #formula MET Value x 3.5 x body weight (kg) / 200 = calories burned per minute
+      @calc = ((@exerciseResult.met * 3.5 * @weightResult) / 200).to_f
+      @minutes = (@foodResult.calories / @calc).round
+      
+      
+      
   end
  end
