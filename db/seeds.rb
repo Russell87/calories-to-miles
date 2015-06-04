2e93af39ecd64f1ef5782cc09774cd3009ada2b4
Russell <russellchristensen@me.com>
2015-06-04 18:26:42 +0000

    Updated CSV file with data proper and tested functionality in database view.

--- db/seeds.rb
+++ db/seeds.rb
@@ -40,8 +40,8 @@ end
 #   category, met, description = row #bring all the stuff out of the csv file so the database will know what to accept
 #    Exercise.create(category: category, met: met, description: description)
 
-    file_path = "#{Rails.root}/lib/data/file.csv"
-    CSV.foreach(file_path, :headers => true, :col_sep => ',') do |row|
+    file_path = "#{Rails.root}/lib/data/NewExerciseDataTest.csv"
+    CSV.foreach(file_path, :headers => true, :col_sep => ",", :row_sep => :auto) do |row|
     Exercise.create(:category => row['category'], :description => row['description'], :met => row['met'])
    
 end 
